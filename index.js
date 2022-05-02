afca765d9ad51200be258b084b14e89de8915a5c
Capwonderbread <hectcast10@yahoo.com>
2022-05-01 21:08:51 -0700

    added team array then formed manager question took answers and push manager in to team array

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatePage = require('./src/generatePage');
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
